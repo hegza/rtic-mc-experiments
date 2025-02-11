b49b9fc8ced2c3e6e4c10d370b543596744aa5c1
Henri Lunnikivi <heggggxa@gmail.com>
2025-02-11 16:41:43 +0200

    test: add idle example

---
+++ atalanta-rtic/example-apps/examples/idle.rs
@@ -0,0 +1,55 @@
+#![no_std]
+#![no_main]
+
+#[rtic::app(device = bsp)]
+mod app {
+    use core::arch::asm;
+
+    use bsp::{fugit::ExtU32, sprintln, timer_group::Timer0, uart::ApbUart, CPU_FREQ};
+
+    #[shared]
+    struct Shared {
+        dummy: bool,
+    }
+
+    #[init]
+    fn init() -> Shared {
+        Shared { dummy: true }
+    }
+
+    #[idle]
+    struct Idle {}
+    impl RticIdleTask for Idle {
+        fn init() -> Self {
+            Self {}
+        }
+        fn exec(&mut self) -> ! {
+            loop {
+                sprintln!("enter sleep");
+                unsafe { asm!("wfi") };
+            }
+        }
+    }
+
+    #[task(binds = Timer0Cmp, priority=1, shared=[dummy])]
+    struct SomeTask {}
+
+    impl RticTask for SomeTask {
+        fn init() -> Self {
+            let _uart = ApbUart::init(CPU_FREQ, 115_200);
+            sprintln!("init");
+
+            let mut timer = Timer0::init().into_periodic();
+            timer.set_period(50.micros());
+            timer.start();
+
+            Self {}
+        }
+
+        fn exec(&mut self) {
+            sprintln!("A");
+            sprintln!("1");
+            sprintln!("B");
+        }
+    }
+}
